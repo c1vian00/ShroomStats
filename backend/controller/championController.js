832d0de3ee808ed025c4623a4f5ea8fcfd7245ff
Jukka Pekkala <jukka@jpekkala.com>
2024-12-13 17:38:00 +0200

    Fetch champions from ddragon

--- backend/controller/championController.js
+++ backend/controller/championController.js
@@ -1,7 +1,29 @@
 import axios from "axios";
 
+const DDRAGON_VERSION = "14.24.1"
+
 const riotToken = process.env.RIOT_API_KEY;
 const baseUrl = process.env.RIOT_API_URL;
+
+async function getChampionMap() {
+    const response = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${DDRAGON_VERSION}/data/en_US/champion.json`)
+    const championMap = {}
+    for (const champion of Object.values(response.data.data)) {
+        championMap[champion.key] = champion
+    }
+    return championMap
+}
+
+function convertToChampion(championId, championMap) {
+    const champion = championMap[championId]
+    return {
+        id: championId,
+        name: champion.name,
+        pictureUrl: `https://ddragon.leagueoflegends.com/cdn/${DDRAGON_VERSION}/img/champion/` + champion.image.full,
+        title: champion.title,
+        blurb: champion.blurb,
+    }
+}
 
 export async function listChampions(req, res, next) {
     try {
@@ -12,9 +34,14 @@ export async function listChampions(req, res, next) {
             },
         });
 
-        res.status(200).json(response.data);
+        const { freeChampionIds } = response.data
+        const championMap = await getChampionMap()
+        const result = {
+            champions: freeChampionIds.map(championId => convertToChampion(championId, championMap))
+        }
+        res.status(200).json(result);
     } catch (error) {
         console.error('Error finding champions', error);
-        res.status(500).json({ error: 'Failed to fetch trending movies' });
+        res.status(500).json({ error: 'Failed to fetch trending champions' });
     }
 }
