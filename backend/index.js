106270f4100b9b4a994156f79b62dd1f12337697
Jukka Pekkala <jukka@jpekkala.com>
2024-12-13 19:11:40 +0200

    Fall back to .env.defaults

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import './dotenvConfig.js'
 import app from './server.js'
-const PORT = process.env.PORT || 3001
+const PORT = process.env.SERVER_PORT
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
